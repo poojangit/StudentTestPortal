fc7fc02f8a46e98563819178ab84ef396fadffc8
poojangit <poojang147@gmail.com>
2024-05-31 23:15:21 +0530

    [Pooja] CREATED: created array of student object

---
+++ studentTestPortal.js
@@ -0,0 +1,177 @@
+var studentDetails = [
+    {
+        Roll_no: 501,
+        Name: "Liam Garcia",
+        Class: 5,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 502,
+        Name: "Ava Robinson",
+        Class: 5,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 503,
+        Name: "Lucas Cooper",
+        Class: 5,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 504,
+        Name: "Emma Reed",
+        Class: 5,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 505,
+        Name: "Mia Hughes",
+        Class: 5,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 601,
+        Name: "Sophia Smith",
+        Class: 6,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 602,
+        Name: "Ethan Johnson",
+        Class: 6,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 603,
+        Name: "Ava Williams",
+        Class: 6,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 604,
+        Name: "Noah Brown",
+        Class: 6,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 605,
+        Name: "Olivia Jones",
+        Class: 6,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 701,
+        Name: "Liam Davis",
+        Class: 7,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 702,
+        Name: "Emma Martinez",
+        Class: 7,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 703,
+        Name: "Mia Wilson",
+        Class: 7,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 704,
+        Name: "Lucas Taylor",
+        Class: 7,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 705,
+        Name: "Aiden Anderson",
+        Class: 7,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 801,
+        Name: "Isabella Thomas",
+        Class: 8,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 802,
+        Name: "James White",
+        Class: 8,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 803,
+        Name: "Avery Clark",
+        Class: 8,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 804,
+        Name: "Mason Turner",
+        Class: 8,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 805,
+        Name: "Charlotte Harris",
+        Class: 8,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 801,
+        Name: "Evelyn Scott",
+        Class: 9,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 802,
+        Name: "Logan King",
+        Class: 9,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 803,
+        Name: "Harper Turner",
+        Class: 9,
+        Gender: "Female",
+        test_score: []
+    },
+    {
+        Roll_no: 804,
+        Name: "Jackson Lee",
+        Class: 9,
+        Gender: "Male",
+        test_score: []
+    },
+    {
+        Roll_no: 805,
+        Name: "Abigail Baker",
+        Class: 9,
+        Gender: "Female",
+        test_score: []
+    }	
+] 
