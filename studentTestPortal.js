7e0dff6e2d6f539c392a24e1ad1095ea09a4ecad
poojangit <poojang147@gmail.com>
2024-06-03 14:30:52 +0530

    [POOJA] Modified: GenerateResult() 's logic merged with TakeTest() (Implemented total marks and percentage in a single function

--- studentTestPortal.js
+++ studentTestPortal.js
@@ -177,40 +177,29 @@ var studentDetails = [
     }
 ]
 let testTaken = false
-let resultGenerated = false
 function mainMenu() {
     console.log("1 Take Test")
-    console.log("2 Generate Result")
-    console.log("3 View Students Result")
-    console.log("4 View Classwise result ")
-    console.log("5 Exit from the choice")
+    console.log("2 View Students Result")
+    console.log("3 View Classwise result ")
+    console.log("4 Exit from the choice")
 
     const choice = readlineSync.question('Please select an option : ')
 
     switch (choice) {
         case '1':
             takeTest();
-            break;
+            break
         case '2':
             if (!testTaken) {
-                console.log("Please take the test first");
-                mainMenu()
-            }
-            else {
-                GenerateResult();
-            }
-            break;
-        case '3':
-            if (!resultGenerated) {
                 console.log("Please Calculate the total marks and percentage first");
                 mainMenu()
             }
             viewStudentsResult();
             break;
-        case '4' :
+        case '3' :
             viewClassWiseResult()
            break
-        case '5':
+        case '4':
             return
         default:
             console.log('Invalid choice. Please select again');
@@ -226,33 +215,21 @@ function generateRandomNum() {
 
 function takeTest() {
     const subjects = ["Java", "Python", "ReactJs"];
-    for (let test of studentDetails) {
-        test.test_score = subjects.map(subject => ({
+    for (let student of studentDetails) {
+        student.test_score = subjects.map(subject => ({
             sub_name: subject,
             marks: generateRandomNum()
         }))
+        let totalMarks = 0
+        student.test_score.forEach(score => {
+            totalMarks += score.marks
+        })
+        const numSubjects = student.test_score.length
+        student.totalMarks = totalMarks
+        student.percentage = (totalMarks/(numSubjects*100))*100
     }
     testTaken = true
-    console.log("Test scores have been generated for all students.");
-    mainMenu()
-}
-function GenerateResult() {
-    studentDetails.forEach(student => {
-        if (student.test_score.length === 0) {
-            console.log(`No test scores available for ${student.Name}`);
-        }
-        else {
-            let totalMarks = 0;
-            student.test_score.forEach(score => {
-                totalMarks += score.marks;
-            });
-            const numSubjects = student.test_score.length;
-            student.totalMarks = totalMarks;
-            student.percentage = (totalMarks / (numSubjects * 100)) * 100;
-        }
-    });
-    resultGenerated = true
-    console.log("Generated the result along with total marks and percentage");
+    console.log("Test scores have been generated for all students. along with total marks and percentage");
     mainMenu()
 }
 
@@ -261,7 +238,7 @@ function viewStudentsResult() {
     const student = studentDetails.find(s => s.Roll_no === roll_no);
     if (student) {
         if (!student.totalMarks || !student.percentage) {
-            console.log(`Results for ${student.Name} are not generated. Please generate the result.`);
+            console.log(`Results for ${student.Name} are not generated. Please take the test.`);
         } else {
             console.log(`Results for ${student.Name}:`);
             student.test_score.forEach(score => {
